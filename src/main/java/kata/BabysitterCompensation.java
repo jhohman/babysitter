8ed65bff6b4b5960d135563c36d73be6030484f8
jhohman <justinhohman@gmail.com>
2018-05-28 11:45:38 -0400

    Refactored to Timesheet
    Refactored the hours calculations to a Timesheet object.

--- src/main/java/kata/BabysitterCompensation.java
+++ src/main/java/kata/BabysitterCompensation.java
@@ -7,62 +7,17 @@ public class BabysitterCompensation {
     private final int hourlyPostMidnightRate = 8;
     
     public int calculate(int arrivalHour, int departureHour, int bedtimeHour) {
-        int childcareHours = getChildcareHours(arrivalHour, departureHour, bedtimeHour);
-        int houseSittingHours = getHouseSittingHours(arrivalHour, departureHour, bedtimeHour);
-        int postMidnightHours = getPostMidnightHours(arrivalHour, departureHour);
+        Timesheet timesheet = new Timesheet(arrivalHour, departureHour, bedtimeHour);
+        int childcareHours = timesheet.getTotalHoursForChildcare();
+        int houseSittingHours = timesheet.getTotalHoursForHouseSitting();
+        int postMidnightHours = timesheet.getTotalHoursForPostMidnight();
         
         return sum(childcareHours, houseSittingHours, postMidnightHours);
-    }
-
-    private int getChildcareHours(int arrivalHour, int departureHour, int bedtimeHour) {
-        if (arrivedAfterBedtime(arrivalHour, bedtimeHour)) {
-            return 0;
-        }
-        return Math.min(bedtimeHour, adjustHour(departureHour)) - arrivalHour;
-    }
-
-    private int getHouseSittingHours(int arrivalHour, int departureHour, int bedtimeHour) {
-        if(arrivedAfterMidnight(arrivalHour)) {
-            return 0;
-        }
-        if (departedBeforeBedtime(departureHour, bedtimeHour)) {
-            return 0;
-        }
-        if (arrivedAfterBedtime(arrivalHour, bedtimeHour)) {
-            return departureHour - arrivalHour;
-        }
-        return 12 - bedtimeHour;
-    }
-
-    private int getPostMidnightHours(int arrivalHour, int departureHour) {
-        if (arrivedAfterMidnight(arrivalHour)) {
-            return departureHour - (arrivalHour == 12 ? 0 : arrivalHour);
-        }
-        return departureHour < 5 ? departureHour : 0;
     }
 
     private int sum(int childcareHours, int houseSittingHours, int postMidnightHours) {
         return (childcareHours * hourlyChildcareRate) 
                 + (houseSittingHours * hourlyHouseSittingRate) 
                 + (postMidnightHours * hourlyPostMidnightRate);
-    }
-
-    private boolean arrivedAfterMidnight(int arrivalHour) {
-        if (arrivalHour == 12 || arrivalHour < 5) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean departedBeforeBedtime(int departureHour, int bedtimeHour) {
-        return adjustHour(departureHour) <= bedtimeHour;
-    }
-
-    private boolean arrivedAfterBedtime(int arrivalHour, int bedtimeHour) {
-        return adjustHour(arrivalHour) >= bedtimeHour ;
-    }
-
-    private int adjustHour(int hour) {
-        return hour < 5 ? hour + 12 : hour;
     }
 }
---
+++ src/main/java/kata/Timesheet.java
@@ -0,0 +1,59 @@
+package kata;
+
+public class Timesheet {
+    private int arrivalHour;
+    private int departureHour;
+    private int bedtimeHour;
+
+    public Timesheet(int arrivalHour, int departureHour, int bedtimeHour) {
+        this.arrivalHour = arrivalHour;
+        this.departureHour = departureHour;
+        this.bedtimeHour = bedtimeHour;
+    }
+
+    public int getTotalHoursForChildcare() {
+        if (arrivedAfterBedtime()) {
+            return 0;
+        }
+        return Math.min(bedtimeHour, adjustHour(departureHour)) - arrivalHour;
+    }
+    
+    public int getTotalHoursForHouseSitting() {
+        if(arrivedAfterMidnight(arrivalHour)) {
+            return 0;
+        }
+        if (departedBeforeBedtime()) {
+            return 0;
+        }
+        if (arrivedAfterBedtime()) {
+            return departureHour - arrivalHour;
+        }
+        return 12 - bedtimeHour;
+    }
+    
+    public int getTotalHoursForPostMidnight() {
+        if (arrivedAfterMidnight(arrivalHour)) {
+            return departureHour - (arrivalHour == 12 ? 0 : arrivalHour);
+        }
+        return departureHour < 5 ? departureHour : 0;
+    }
+
+    private boolean arrivedAfterMidnight(int arrivalHour) {
+        if (arrivalHour == 12 || arrivalHour < 5) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean departedBeforeBedtime() {
+        return adjustHour(departureHour) <= bedtimeHour;
+    }
+
+    private boolean arrivedAfterBedtime() {
+        return adjustHour(arrivalHour) >= bedtimeHour ;
+    }
+
+    private int adjustHour(int hour) {
+        return hour < 5 ? hour + 12 : hour;
+    }
+}
