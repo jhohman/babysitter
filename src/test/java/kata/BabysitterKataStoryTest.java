85e1a1febeebecba6211dbad37f757a4733851ac
jhohman <justinhohman@gmail.com>
2018-05-27 21:42:50 -0400

    Pull babysitterCompensation up.

--- src/test/java/kata/BabysitterKataStoryTest.java
+++ src/test/java/kata/BabysitterKataStoryTest.java
@@ -4,11 +4,10 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class BabysitterKataStoryTest {
+    public BabysitterCompensation babysitterCompensation = new BabysitterCompensation();
     
     @Test
     public void calculateCompensatation_For_MinArrival_MaxDeparture_MaxBedtime() {
-        BabysitterCompensation babysitterCompensation = new BabysitterCompensation();
-
         int result = babysitterCompensation.calculate(5,4,12);
         
         assertThat(result).isEqualTo(102);
@@ -16,8 +15,6 @@ public class BabysitterKataStoryTest {
     
     @Test
     public void calculateCompensation_BedtimeBeforeMidnight() {
-        BabysitterCompensation babysitterCompensation = new BabysitterCompensation();
-
         int result = babysitterCompensation.calculate(6, 12, 9);
         
         assertThat(result).isEqualTo(48);
@@ -25,7 +22,6 @@ public class BabysitterKataStoryTest {
     
     @Test
     public void calculateCompensation_DepartBeforeBedtime() {
-        BabysitterCompensation babysitterCompensation = new BabysitterCompensation();
         int result = babysitterCompensation.calculate(8, 9, 10);
 
         assertThat(result).isEqualTo(10);
@@ -33,8 +29,6 @@ public class BabysitterKataStoryTest {
     
     @Test
     public void calculateCompensation_ArriveAfterBedtime() {
-        BabysitterCompensation babysitterCompensation = new BabysitterCompensation();
-
         int result = babysitterCompensation.calculate(10, 11, 9);
 
         assertThat(result).isEqualTo(6);
@@ -42,10 +36,9 @@ public class BabysitterKataStoryTest {
     
     @Test
     public void calculateCompensation_ArriveAtBedtime() {
-        BabysitterCompensation babysitterCompensation = new BabysitterCompensation();
-
         int result = babysitterCompensation.calculate(9, 11, 9);
 
         assertThat(result).isEqualTo(12);
     }
+
 }
